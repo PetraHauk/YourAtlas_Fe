e855d24a723404de8506aa887dd48becb6abb0f6
PetraHauk <petra.haukkala@gmail.com>
2025-06-15 21:08:35 +0300

    Logout

--- Js-template/js/main.js
+++ Js-template/js/main.js
@@ -1,3 +1,12 @@
-function logout(){
-
+function logout() {
+  localStorage.clear();
+  location.href="index.html";
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  const logoutLink = document.querySelector("a.logout-link");
+
+  if (logoutLink) {
+    logoutLink.addEventListener("click", logout);
+  }
+})
