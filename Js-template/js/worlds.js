60e03540ff70d7a53a702640a98d725936147883
Nappulat <neazuski@gmail.com>
2025-06-15 22:23:13 +0300

    Add a function for posting a new world.

--- Js-template/js/worlds.js
+++ Js-template/js/worlds.js
@@ -7,6 +7,46 @@ function printUsername () {
   heading.innerHTML = `${username}`;
 }
 
+// Function for posting a new world
+function registerWorld(e) {
+  e.preventDefault();
+  //Get correct form
+  const worldForm = document.querySelector('#postNewWorld');
+  //Ger values
+  const world_name = worldForm.querySelector('#new-world-name').value.trim();
+  // SE NYT OTTAA DEFAULTTINA TON ONGOING MANUAALISESTI, EN JAKSANUT NYT MIETTII TOTA LOGIIKKAA ET SE OTTAA OIKEEN
+  // JOS TESTAAT NI VALITSE AINA ONGOING
+  const status = worldForm.querySelector('#Ongoing-story').value.trim();
+  const user_id = localStorage.getItem('user_id')
+  // Data for request
+  const bodyData = {
+      world_name: world_name,
+      state: status
+  };
+  console.log(bodyData)
+  // Endpoint
+  const url = `http://localhost:3000/api/worlds/${user_id}`;
+  let token = localStorage.getItem('authToken');
+  // Options
+  const options = {
+      body: JSON.stringify(bodyData),
+      method: 'POST',
+      headers: {
+      'Authorization': `Bearer ${token}`,
+      'Content-type': 'application/json',
+      },
+  };
+  // Send a request for posting data
+  const response = fetch(url, options);
+  if (response.error) {
+      console.error('Creating A New World Failed:', response.error);
+      return;
+    } else {
+      alert('World was saved!')
+    }
+    worldForm.reset();
+};
+
 // THIS IS FOR THE MOCK DATA
 function appendCreateNewWorld(worlds) {
   return [
@@ -132,3 +172,5 @@ document.addEventListener("DOMContentLoaded", () => {
   document.getElementById("scroll-left").addEventListener("click", () => scrollWorlds(-1));
   document.getElementById("scroll-right").addEventListener("click", () => scrollWorlds(1));
 });
+
+document.getElementById("input-newest-world").addEventListener("click", () => registerWorld(e));
